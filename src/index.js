0a9a728c78fc15d0fc846ddb0257f398767ea893
felixcapuano <felix.capuano@gmail.com>
2021-08-03 14:51:58 +0200

    instance matomo

--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App/App';
 
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
+
+const matomo = createInstance({
+  urlBase: 'https://localhost:8080/',
+  siteId: 1,
+  userId: 'UID76903202', // optional, default value: `undefined`.
+  //   trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
+  //   srcUrl: 'https://LINK.TO.DOMAIN/tracking.js', // optional, default value: `${urlBase}matomo.js`
+  //   disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+  //   heartBeat: { // optional, enabled by default
+  //     active: true, // optional, default value: true
+  //     seconds: 10 // optional, default value: `15
+  //   },
+  //   linkTracking: false, // optional, default value: true
+  //   configurations: { // optional, default value: {}
+  //     // any valid matomo configuration, all below are optional
+  //     disableCookies: true,
+  //     setSecureCookie: true,
+  //     setRequestMethod: 'POST'
+  //   }
+});
+
 ReactDOM.render(
+  <MatomoProvider value={matomo}>
     <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
+      <App />
+    </Router>
+  </MatomoProvider>,
+  document.getElementById('root')
 );
