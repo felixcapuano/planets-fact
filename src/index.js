ce5083876970a6f311d1e199d93816c7903f5b9b
MammothOG <felix.capuano@gmail.com>
2021-08-19 17:43:12 +0200

    index

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ const matomo = createInstance({
   userId: uuidv4(), // optional, default value: `undefined`.
   //   trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
   //   srcUrl: 'https://LINK.TO.DOMAIN/tracking.js', // optional, default value: `${urlBase}matomo.js`
-    disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+  disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
   //   heartBeat: { // optional, enabled by default
   //     active: true, // optional, default value: true
   //     seconds: 10 // optional, default value: `15
   //   },
-  //   linkTracking: false, // optional, default value: true
+  linkTracking: false, // optional, default value: true
   //   configurations: { // optional, default value: {}
   //     // any valid matomo configuration, all below are optional
   //     disableCookies: true,
